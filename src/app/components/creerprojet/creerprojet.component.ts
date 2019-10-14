a41a10245aa5e2bd81055c65b9f44132a169b9cb
Alexandre <alexcordev@gmail.com>
2019-10-14 17:32:05 -0400

    About and Create Section finished

--- src/app/components/creerprojet/creerprojet.component.ts
+++ src/app/components/creerprojet/creerprojet.component.ts
@@ -1,59 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import {Project} from '../../models/project';
-import {ProjectService} from '../../services/project.service';
-import {UploadService} from '../../services/upload.service';
-import {global} from '../../services/global';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+import { UploadService } from '../../services/upload.service';
+import { global } from '../../services/global';
 @Component({
   selector: 'app-creerprojet',
   templateUrl: './creerprojet.component.html',
   styleUrls: ['./creerprojet.component.css'],
-  providers: [ProjectService, UploadService]
+  providers: [
+    ProjectService,
+    UploadService
+  ]
 })
 export class CreerprojetComponent implements OnInit {
+  public title: string;
+  public project: Project;
+  public status: string;
+  public filesToUpload: Array<File>;
 
-    public title: string;
-    public project: Project;
-    public status: string;
-    public filesToUpload: Array<File>;
-
-    constructor(
-      private projectService: ProjectService,
-      private uploadService: UploadService
-    ) {
-
-      this.title = 'Créer un projet';
-      this.project = new Project('', '', '', '', 2019, '', '');
-     }
-
-    ngOnInit() {
+  constructor(
+    private projectService: ProjectService,
+    private uploadService: UploadService
+  ) {
+    this.title = 'Créer un projet';
+    this.project = new Project('', '', '', '', 2019, '', '');
   }
-
+  ngOnInit() {
+  }
   onSubmit(form) {
-    console.log(this.project);
+
+    // GUARDAR LOS DATOS
     this.projectService.saveProject(this.project).subscribe(
       response => {
-          if (response.project) {
-            this.uploadService.makeFileRequest(global.url + 'upload-image/' + response.project.id, [], this.filesToUpload, 'files' )
-            .then((result: any) => {
+        if (response.project) {
+          // SUBIR LA IMAGEN
+          this.uploadService.makeFileRequest(global.url + 'upload-image/' + response.project._id, [ ], this.filesToUpload, 'files')
+          .then((result: any) => {
+            this.status = 'success';
+            console.log(result);
+            form.reset();
+          });
+        } else {
 
-              console.log(result);
-              this.status = 'success';
-              form.reset();
-            });
+          this.status = 'failed';
+        }
 
-          } else {
-
-            this.status = 'failed';
-          }
-
-      },
-      error => {
-            console.log(error);
-      }
-    );
-  }
+    },
+    error => {
+          console.log(error);
+    }
+  );
+}
 
   fileChangeEvent(fileInput: any) {
-      this.filesToUpload = fileInput.target.files;
+    this.filesToUpload = fileInput.target.files as Array<File>;
   }
+
 }
